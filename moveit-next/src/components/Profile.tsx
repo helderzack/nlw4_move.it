6e3bb79c972941d8c15a28ca73729f62e1f40938
Helder_Zacarias <helderacircletool@@outlook.com>
2021-02-25 11:18:14 -0300

    Aula 04

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/helderzack.png" alt="Hélder"/>
             <div>
                 <strong>Hélder Zacarias</strong>
-                <p><img src="icons/level.svg" alt="Level"/>Level 1</p>
+                <p><img src="icons/level.svg" alt="Level"/>Level {level}</p>
             </div>
         </div>
     );
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -6,23 +6,30 @@ import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
 
+import { CountdownProvider } from '../contexts/CountdownContext';
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Início | mvove.it</title>
       </Head>
+
       <ExperienceBar/>
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-        </div>
-        <div>
-          <ChallengeBox/>
-        </div>
-      </section>
+
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile/>
+            <CompletedChallenges/>
+            <Countdown/>
+          </div>
+          <div>
+            <ChallengeBox/>
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
+
   )
 }
