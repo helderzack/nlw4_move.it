457a511c267caa7610bce8f363fd9800e56e8d03
Helder_Zacarias <helderacircletool@@outlook.com>
2021-02-25 00:11:13 -0300

    AULA 03

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
           <Countdown/>
         </div>
         <div>
-
+          <ChallengeBox/>
         </div>
       </section>
     </div>
